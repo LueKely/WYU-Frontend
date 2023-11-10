7f98eb018dad47c9635f8618ab8efa639cd1fd9a
LueKely <99306129+LueKely@users.noreply.github.com>
2023-11-10 13:26:46 +0800

    feat: Added login page

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>icon</div>
+  <h4>Sign in</h4>
+  <q-form ref="form" greedy @submit="login">
+    <q-input
+      class="font"
+      rounded
+      outlined
+      v-model="email"
+      label="Email"
+      type="email"
+      lazy-rules
+      :rules="[(val) => !!val || 'Password is required']"
+    />
+    <q-input
+      class="font"
+      rounded
+      outlined
+      v-model="password"
+      label="Password"
+      type="password"
+      lazy-rules
+      :rules="[(val) => !!val || 'Password is required']"
+    />
+    <q-btn type="submit" label="Login" />
+  </q-form>
+</template>
+
+<script setup lang="js">
+import { ref } from 'vue';
+const form = ref(null);
+const email = ref('');
+const password = ref('');
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-page class="page q-pa-xl">
+    <!-- form -->
+    <div class="login login--form">
+      <LoginForm></LoginForm>
+    </div>
+
+    <!-- carousel -->
+    <div class="login login--carousel">carousel</div>
+  </q-page>
+</template>
+
+<script setup lang="js">
+import LoginForm from 'src/components/LoginForm.vue';
+</script>
+
+<style lang="scss" scoped>
+.page {
+  width: 100dvw;
+  height: 100dvh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.page--login {
+  padding: 20px;
+}
+.login {
+  padding: 15px;
+  border-radius: 15px;
+}
+.login--form {
+  margin-right: 10px;
+  height: 100%;
+  background-color: rgb(206, 160, 160);
+  flex: 1;
+}
+.login--carousel {
+  margin-left: 10px;
+  height: 100%;
+  background-color: rgb(145, 145, 231);
+  flex: 2;
+}
+</style>
