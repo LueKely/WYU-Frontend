47cd65fdeb95b2fe0b17e1a12b9b760ff1bbfc60
LueKely <99306129+LueKely@users.noreply.github.com>
2023-11-10 15:13:19 +0800

    FEAT: loginpage style

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,28 +1,44 @@
 <template>
-  <div>icon</div>
-  <h4>Sign in</h4>
+  <div class="column items-center justify-center q-mb-xl">
+    <q-icon class="q-my-lg" name="img:/wyu-icon.svg" size="10rem" />
+    <h4 class="q-my-lg">Sign in</h4>
+  </div>
+
   <q-form ref="form" greedy @submit="login">
+    <label for="username" class="font text-18 q-mb-sm">Username/Email</label>
     <q-input
-      class="font"
-      rounded
+      id="username"
+      class="font q-mb-sm"
       outlined
       v-model="email"
-      label="Email"
       type="email"
       lazy-rules
-      :rules="[(val) => !!val || 'Password is required']"
+      :rules="[(val) => !!val || 'username is required']"
     />
+    <label for="password" class="font text-18 q-mb-sm">Password</label>
     <q-input
-      class="font"
-      rounded
+      id="password"
+      class="font q-mb-sm"
       outlined
       v-model="password"
-      label="Password"
       type="password"
       lazy-rules
       :rules="[(val) => !!val || 'Password is required']"
     />
-    <q-btn type="submit" label="Login" />
+    <q-btn
+      color="accent"
+      size="lg"
+      class="font-bold q-my-sm login__button"
+      type="submit"
+      label="Login"
+    />
+    <q-btn
+      color="primary"
+      outline
+      size="lg"
+      class="font-bold q-my-sm login__button login--signup"
+      label="Sign Up"
+    />
   </q-form>
 </template>
 
@@ -33,4 +49,11 @@ const email = ref('');
 const password = ref('');
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.login__button {
+  width: 100%;
+}
+.login--signup {
+  border-color: red;
+}
+</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-page class="page q-pa-xl">
+  <q-page class="page page--login q-pa-xl">
     <!-- form -->
-    <div class="login login--form">
+    <div class="login login--form shadow-3">
       <LoginForm></LoginForm>
     </div>
 
     <!-- carousel -->
-    <div class="login login--carousel">carousel</div>
+    <div class="login login--carousel shadow-3">carousel</div>
   </q-page>
 </template>
 
@@ -23,22 +23,35 @@ import LoginForm from 'src/components/LoginForm.vue';
   justify-content: center;
 }
 .page--login {
-  padding: 20px;
+  position: relative;
+  background-image: linear-gradient(
+    to right bottom,
+    #f2d366,
+    #f6bd50,
+    #f9a63f,
+    #fc8d35,
+    #fd7133
+  );
+}
+
+.page--login::after {
+  content: "";
 }
 .login {
   padding: 15px;
   border-radius: 15px;
+  background-color: white;
 }
+
 .login--form {
-  margin-right: 10px;
   height: 100%;
-  background-color: rgb(206, 160, 160);
-  flex: 1;
+  width: 100%;
+  flex: 0.5;
+  margin-right: 10px;
 }
 .login--carousel {
   margin-left: 10px;
   height: 100%;
-  background-color: rgb(145, 145, 231);
   flex: 2;
 }
 </style>
