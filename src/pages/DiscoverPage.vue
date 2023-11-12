da0f33aefc2a14ad05b0a535b78211ca5a4f8d92
LueKely <99306129+LueKely@users.noreply.github.com>
2023-11-12 13:02:21 +0800

    Init: home page

---
+++ src/pages/DiscoverPage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page--discover">
+    <div class="container container-sections">
+      <section>Ulam of the day</section>
+      <section>Recent Posts</section>
+      <section>Categories</section>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.page--discover {
+  width: 100%;
+  height: 100%;
+
+  background-color: rgb(230, 199, 199);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.container-sections {
+  padding: 0;
+  box-sizing: border-box;
+  width: 60%;
+  height: 100%;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  flex-direction: column;
+}
+</style>
