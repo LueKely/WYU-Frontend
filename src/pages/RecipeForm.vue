9c79d073e4f0708456aab76b7ec38969947dcc7f
LueKely <99306129+LueKely@users.noreply.github.com>
2023-11-17 15:14:34 +0800

    todo list in receipe form

--- src/pages/RecipeForm.vue
+++ src/pages/RecipeForm.vue
@@ -1,6 +1,13 @@
 <template>
   <div>Recipe Form</div>
   <q-form ref="form" greedy @submit="sendForm"> </q-form>
+  <!-- name input -->
+  <!-- description input -->
+  <!-- difficulty dropdown -->
+  <!-- time to make timer button -->
+  <!-- tags drop down with plus button -->
+  <!-- ingridients add button thingy-->
+  <!-- instructions text box add text box button -->
 </template>
 
 <script setup></script>
