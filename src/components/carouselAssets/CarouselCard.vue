7800c451d587bb271f579ac3f6328092c142c682
LueKely <99306129+LueKely@users.noreply.github.com>
2023-11-21 09:59:26 +0800

    Finished card css

--- src/components/carouselAssets/CarouselCard.vue
+++ src/components/carouselAssets/CarouselCard.vue
@@ -4,11 +4,11 @@
       src="https://picsum.photos/500/300"
       :ratio="16 / 9"
       loading="true"
-      width="270px"
+      width="100%"
       height="150px"
     />
     <div class="card__container--text">
-      <h4 class="text-24 font-bold">Lorem Burger</h4>
+      <h4 class="text-18 font-bold">Lorem Burger</h4>
       <p class="card__text">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus dolor
         nemo explicabo repellendus necessitatibus adipisci veritatis harum,
@@ -24,12 +24,12 @@
 <style lang="scss" scoped>
 .card__container {
   background: #ffe2b8;
-  width: 300px;
-  height: 270px;
+  width: 310px;
+  height: 290px;
   border-radius: 10px;
   display: flex;
   align-items: flex-start;
-  justify-content: center;
+  justify-content: flex-start;
   flex-direction: column;
   padding: 15px;
 }
@@ -40,10 +40,11 @@
   width: 100%;
 }
 .card__text {
+  margin: 0;
   overflow: hidden;
   display: -webkit-box;
-  -webkit-line-clamp: 2; /* number of lines to show */
-  line-clamp: 2;
+  -webkit-line-clamp: 3; /* number of lines to show */
+  line-clamp: 3;
   -webkit-box-orient: vertical;
 }
 </style>
