ec7fbb2b2e7db5cab47aeaecb015fc14e58df701
LueKely <luekely43110@gmail.com>
2023-12-21 21:47:33 +0800

    feat: scaffolding of html elemetns

---
+++ src/components/profileAssets/UserImage.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container__profile--image">
+    <div class="container__profile--banner">
+      <q-img
+        src="https://picsum.photos/500/300"
+        height="300px"
+        :ratio="4 / 3"
+      />
+    </div>
+    <div class="container__profile--avatar">
+      <q-avatar
+        size="100px"
+        font-size="52px"
+        color="teal"
+        text-color="white"
+        icon="directions"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container__profile--image {
+  padding-inline: 20px;
+  background-color: transparent;
+  height: 352px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+}
+.container__profile--banner {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  height: 300px;
+  width: 100%;
+  aspect-ratio: 4/3;
+  background-color: red;
+}
+
+.container__profile--avatar {
+}
+</style>
--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,9 +1,46 @@
 <template>
   <div class="page page--search">
-    <div class="container--search"></div>
+    <div class="container--profile">
+      <UserImage />
+      <div class="container--split">
+        <!-- insert user links here -->
+        <div class="container__user--info">
+          <!-- <UserLinks /> -->
+        </div>
+        <div class="container__user--activity">
+          <!-- navigation for the two -->
+          <div class="container__controls"></div>
+          <!-- insert the two here-->
+          <div>
+            <!--
+            <UserCollections />
+            <UserLikedPost /> -->
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import UserImage from "@profile/UserImage.vue";
+import UserLinks from "@profile/UserLinks.vue";
+import UserCollections from "@profile/UserCollections.vue";
+import UserLikedPost from "@profile/UserLikedPost.vue";
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container--profile {
+  padding-block: 90px 100px;
+  flex: 1;
+  box-shadow: 0px 0px 20px 0px rgba(206, 206, 206, 0.932);
+  background-color: #f5f5f5;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+
+  width: 60%;
+}
+</style>
