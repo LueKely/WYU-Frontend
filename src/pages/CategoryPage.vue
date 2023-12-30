5f1c4b7f6b7ba38552973b5a4ed0f32d3b3cf34d
LueKely <luekely43110@gmail.com>
2023-12-30 09:35:57 +0800

    FEAT: Categories Page (#44)

--- src/pages/CategoryPage.vue
+++ src/pages/CategoryPage.vue
@@ -5,9 +5,33 @@
     leave-active-class="animated fadeOut"
     :duration="2000"
   >
-    <div class="page search">
-      Test
-      <q-inner-loading :showing="pageLoadingState" color="accent-2" />
+    <div class="page page--search">
+      <div class="container--search">
+        <div class="search__container--text">
+          <h3 class="font-bold text-32">Category: {{ category }}</h3>
+        </div>
+        <hr style="width: 100%; color: #f7f2f2; margin-bottom: 10px" />
+        <div class="result__container">
+          <!-- if empty result from fetching -->
+          <div
+            v-if="!recipeLists || recipeLists.length == 0"
+            class="results--error"
+          >
+            <h3 class="text-24">No recipes yet</h3>
+            <p>
+              Looks like we don't have what you are looking for <br />
+              how about you create the recipe and share it with the world?
+            </p>
+          </div>
+
+          <div v-else class="card__container--search">
+            <div v-for="(object, index) in recipeLists" :key="index">
+              <SearchCard v-bind="object"></SearchCard>
+            </div>
+          </div>
+        </div>
+        <q-inner-loading :showing="pageLoadingState" color="accent-2" />
+      </div>
     </div>
   </transition>
 </template>
@@ -16,7 +40,7 @@
 import { ref, onMounted } from "vue";
 import { GetRecipeByCategory } from "@composables/Recipe";
 import { useRoute } from "vue-router";
-
+import SearchCard from "../components/searchAssets/SearchCard.vue";
 /**
  * TODO: Mga need gawin
  * 1. UI for category page
