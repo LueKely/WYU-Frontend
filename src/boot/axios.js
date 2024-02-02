39d0f9982d594b985f4f19821b06779eb1d3481d
LueKely <luekely43110@gmail.com>
2024-02-02 21:58:29 +0800

    adjusted: cors nightmares

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from "quasar/wrappers";
 import axios from "axios";
 
 const axiosConfig = {
-  baseURL: "http://localhost:3001/api/",
+  baseURL: "http://localhost:3000/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
